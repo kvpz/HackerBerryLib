4b02e9f83011a71b805adf5cd4af7b91af9b5171
Kevin Perez <unknotted26@gmail.com>
2017-04-04 18:23:39 -0400

    Trie challenge, Contacts, completed.

---
+++ HackerLibrary/Contacts.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace HackerLibrary
+{
+    public class Contacts
+    {
+        const string SampleInputFile = "..\\..\\Input\\test3.txt";
+        private const string SampleOutputFile = "..\\..\\Output\\Output3.txt";
+
+        public void Add(Dictionary<string, int> dictionary, string input)
+        {
+            for(int j = 1; j < input.Length; ++j)
+            {
+                string substring = input.Substring(0, j);
+
+                if (!dictionary.ContainsKey(substring))
+                    dictionary.Add(substring, 1);
+                else
+                    dictionary[substring] += 1; // increment occurence counter
+            }
+        }
+
+        public KeyValuePair<string, int>? Find(Dictionary<string, int> dictionary, string searchWord)
+        {
+            if(dictionary.ContainsKey(searchWord))
+            {
+                return new KeyValuePair<string, int>(searchWord, dictionary[searchWord]);
+            }
+
+            return null;
+        }
+
+        public void Process(string input = SampleInputFile)
+        {
+            StreamReader stream = new StreamReader(File.OpenRead(input));
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // The first line of the file denotes the total amount of lines that follow
+            int totalLines = Int32.Parse(stream.ReadLine());
+            while(!stream.EndOfStream)
+            {
+                string[] line = stream.ReadLine().Split(' ');
+                string action = line[0];
+                string word = line[1];
+                if(line[0] == "add") // add the item to the dictionary
+                {
+                    Add(dictionary, word);
+                }
+                else if(line[0] == "find") // find the item in the dictionary
+                {
+                    KeyValuePair<string, int>? dictionaryItem = Find(dictionary, word);
+                }
+            }
+
+            stopwatch.Stop();
+
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace HackerRank
 {
-    class Program
+    using System.Diagnostics;
+    class Solution
     {
+        static string[] identifier = { "add", "find" }; // add name, find partial
+
+        static System.IO.FileStream GetInput()
+        {
+            System.IO.FileStream fstream = new System.IO.FileStream("../Output/Output3.txt", FileMode.Open, FileAccess.Read);
+            return fstream;
+        }
+
+        static string outFileName = "..\\..\\Output\\Output3.txt";
+        static string inFileName = "..\\..\\Input\\test3.txt";
+
         static void Main(string[] args)
         {
+            StreamReader sreader = new StreamReader(inFileName);
+            
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int totalLines = Int32.Parse(sreader.ReadLine());
+
+            while(!sreader.EndOfStream)
+            {
+                string[] in_ = sreader.ReadLine().Split(' ');
+                if(in_[0][0] == 'a') // add
+                {
+                    for(int j = 1; j <= in_[1].Length; ++j)
+                    {
+                        string sub = in_[1].Substring(0, j);
+
+                        if (!dictionary.ContainsKey(sub))
+                            dictionary.Add(sub, 1);
+                        else
+                            dictionary[sub] += 1;
+                    }
+                }
+                else // find
+                {
+                    if (!dictionary.ContainsKey(in_[1]))
+                        Console.WriteLine('0');
+                    else
+                        Console.WriteLine(dictionary[in_[1]]);
+
+                }
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"Time: {stopwatch.Elapsed}");
         }
     }
 }
